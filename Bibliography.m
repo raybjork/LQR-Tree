89bf8f89eba300e8909db38f3a0edf38897f179a
raybjork <raybjork@gmail.com>
2020-02-18 11:19:16 -0500

    Removing unnecessary files

--- Bibliography.m
+++
@@ -1,2 +0,0 @@
-http://groups.csail.mit.edu/robotics-center/public_papers/Reist10.pdf
-http://glassmanlab.seas.harvard.edu/papers/quadRegICRA.pdf
--- unused/BuildLQRTree.m
+++
@@ -1,4 +0,0 @@
-function [TreeHead] = BuildLQRTree(system,goalState)
-
-end
-
